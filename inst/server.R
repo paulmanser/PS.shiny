819fb9bf0bc267207b600e2e21dc33675b2ff58a
jeffreyhorner <jeffrey.horner@gmail.com>
2013-12-17 14:48:12 -0600

    Adding t-test interface

--- inst/server.R
+++ inst/server.R
@@ -1,4 +1,9 @@
 library(shiny)
 
+htmlfrag <- function(fname) renderText({
+  paste(readLines(system.file(sprintf('www/%s',fname),package='PS.shiny')),collapse="\n")
+})
+
 shinyServer(function(input, output) {
+  output$`ttest` <- htmlfrag('ttest.html')
 })
